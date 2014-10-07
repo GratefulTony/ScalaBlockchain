5aa71ed2dcf2939d9a40971136bea8797aa002f9
GratefulTony <anthonyjschulte@gmail.com>
2014-10-07 00:05:31 -0600

    update pom

--- src/main/scala/ScalaBlockchain/blockchain/api/BlockchainApi.scala
+++ src/main/scala/ScalaBlockchain/blockchain/api/BlockchainApi.scala
@@ -8,7 +8,6 @@ import ScalaBlockchain.blockchain.transaction.Transaction
  * User: tony
  * Date: 9/13/14
  * Time: 9:14 PM
- * To change this template use File | Settings | File Templates.
  */
 trait BlockchainApi {
   def getTransactions(a:Address):Seq[Transaction]
